d5152bab962a4c205d466f1bcbf410b77bea7808
Marcelo Peixoto de Abreu <marcelopeixotodeabreu@gmail.com>
2024-10-15 14:37:36 -0300

    Ajustes da implementação

--- src/Application.java
+++ src/Application.java
@@ -5,11 +5,11 @@ public class Application {
 	public static void main(String[] args) {
 		 String texto;
 	        Scanner sc = new Scanner(System.in);
-	        System.out.print("Digite uma string: ");
+	        System.out.println("Digite uma string: ");
 	        texto = sc.nextLine();
 
 	        if (Caracteres.palavraDigita(texto)) {
-	            System.out.println("A string - " + texto + " - possui a letra 'a'!");
+	            System.out.println("A string - " + texto + " - possui a letra 'a'!" );
 	        } else {
 	            System.out.println("A string - " + texto + " - NÃO possui a letra 'a'!");
 	        }
--- src/Caracteres.java
+++ src/Caracteres.java
@@ -2,14 +2,15 @@
 public class Caracteres {
 	public static boolean palavraDigita(String caracteres) {
 
-		int quantidadeLetraA = 0;
+		  int quantidadeLetraA = 0;
 
-		for (int i = 0; i < caracteres.length(); i++) {
-			if (caracteres.charAt(i) == 'a' || caracteres.charAt(i) == 'A') {
-				quantidadeLetraA++;
-			}
-		}
-		System.out.println("Quantidade de letras 'a' : " + quantidadeLetraA);
-		return quantidadeLetraA > 0;
-	}
+	        for (int i = 0; i < caracteres.length(); i++) {
+	            if (caracteres.charAt(i) == 'a' || caracteres.charAt(i) == 'A') {
+	                quantidadeLetraA++;
+	            }
+	        }
+	        System.out.println("A letra 'a' aparece " + quantidadeLetraA + " vezes!");
+	        return quantidadeLetraA > 0;
+	    }
+	
 }
